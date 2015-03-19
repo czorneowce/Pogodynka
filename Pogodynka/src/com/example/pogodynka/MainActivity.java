52172d0065a6e764877e6f9ce346e1ccf05198ad
czorneowce <blacksheep525@gmail.com>
2015-03-19 14:36:11 +0100

    sss

--- Pogodynka/src/com/example/pogodynka/MainActivity.java
+++ Pogodynka/src/com/example/pogodynka/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 		//https://dribbble.com/shots/1980005--CSS-Menu-animation?list=shots&sort=popular&timeframe=now&offset=6
 		//sssss
 		//,,,ddd
+		//dddd
 
 	}
 
