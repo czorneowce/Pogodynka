74f5465e48090acf2093f218c83ffbd5dc6791ee
Xaaq <Xaaq333@gmail.com>
2015-03-19 22:44:37 +0100

    dd

--- Pogodynka/src/com/example/pogodynka/MainActivity.java
+++ Pogodynka/src/com/example/pogodynka/MainActivity.java
@@ -3,16 +3,37 @@ package com.example.pogodynka;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	ImageView kolko;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		
+		kolko = (ImageView) findViewById(R.id.kolko);
 
+		final MainActivity that = this;
+		
+		kolko.setOnTouchListener(new View.OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				Animation anim = new AnimationUtils().loadAnimation(that, R.anim.zwieksz_kolo);
+				kolko.startAnimation(anim);
+				return false;
+			}
+		});
+		
 		//https://dribbble.com/shots/1980005--CSS-Menu-animation?list=shots&sort=popular&timeframe=now&offset=6
 	}
 
